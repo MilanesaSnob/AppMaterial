e6d2e404bb9b167994b27521836a3ed565c497c4
Martin Mele <mmmele@gmail.com>
2020-10-07 17:12:15 -0300

    Add project files.

---
+++ AppMaterial/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMaterial
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            XF.Material.Forms.Material.Init(this, "Material.Configuration");
+            MainPage = new MainPage();
+           
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppMaterial/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppMaterial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOnClicked (object sender, EventArgs e)
+        {
+            string text = MainEntry.Text;
+            MainLabel.Text = "Hello " + text;
+        }
+    }
+}
